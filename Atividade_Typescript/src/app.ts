cd6e245b9ab3716e74c2aa169aec06e72b712037
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-07 12:07:55 -0300

    Delete Atividade_Typescript directory

--- Atividade_Typescript/src/app.ts
+++
@@ -1,5 +0,0 @@
-import { Inventario } from './classes/Inventario';
-import { exibirMenu } from './utils/exibirMenu';
-
-const inventario = new Inventario();
-exibirMenu(inventario);
--- Atividade_Typescript/src/interfaces/ICategoria.ts
+++
@@ -1,6 +0,0 @@
-export interface ICategoria {
-    id: string;
-    nome: string;
-    descricao: string;
-    dataCriacao: Date;
-  }  
