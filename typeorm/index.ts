5f8ef452a48428ec191d0899a288aa848dbb697d
Yasmin Silva <133078071+yasminsilva30@users.noreply.github.com>
2025-04-08 23:01:09 -0300

    Delete typeorm directory

--- typeorm/index.ts
+++
@@ -1,9 +0,0 @@
-import { AppDataSource } from "./database/data-source";
-import { exibirMenu } from "./menu";
-
-AppDataSource.initialize()
-    .then(() => {
-        console.log("Banco de dados conectado!");
-        exibirMenu();
-    })
-    .catch((error) => console.log(error));
